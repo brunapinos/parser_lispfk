1ee63a6171410e2ba987c2b1cd2c4e47c424b075
Bruna Pinos <bruna.pinos@gmail.com>
2017-11-21 22:24:16 -0200

    [COMENTARIO] Ignorando comentarios
    Signed-off-by: Bruna Pinos <bruna.pinos@gmail.com>
    Signed-off-by: Guilherme Augusto <guilherme.francais@gmail.com>

--- parser_lispfk.py
+++ parser_lispfk.py
@@ -1,47 +1,52 @@
+from sys import argv
+import pprint 
 import ox
-import click
 
-number = lambda x: ('value', float(x))
-name = lambda y: ('value', str(x))
-brackets = lambda openbracket, term, closebracket: (openbracket, term, closebracket) 
+file_name, lf_source = argv
+
+in_file = open(lf_source)
+source = in_file.read()
 
 lexer = ox.make_lexer([
+    ('COMMENT', r';(.)*'),
+    ('NEW_LINE', r'\n+'),
+    ('NAME', r'[a-zA-Z_][a-zA-Z_0-9-]*'),
     ('NUMBER', r'\d+(\.\d*)?'),
-    ('NAME', r'[a-zA-Z]+'),
     ('OPEN_BRACKET', r'\('),
     ('CLOSE_BRACKET', r'\)'),
 ])
 
 token_list = [
-    'NUMBER',
     'NAME',
+    'NUMBER',
     'OPEN_BRACKET',
     'CLOSE_BRACKET',
 ]
 
 parser = ox.make_parser([
 
-    ('expression : OPEN_BRACKET term CLOSE_BRACKET', brackets),
-    ('term : atom term', lambda atom, term: (atom, term)),
+
+    ('expr : OPEN_BRACKET term CLOSE_BRACKET', lambda openbracket, \
+        atom, closebracket: atom),
+    ('term : term term', lambda term, other_term: (term, other_term)),
     ('term : term atom', lambda term, atom: (term, atom)),
-    ('term : atom', lambda term: term),
-    ('atom : NAME', name),
-    ('atom : NUMBER', number),
+    ('term : atom term', lambda atom, term: (atom, term)),
+    ('term : term', lambda term: term),
+    ('term : atom', lambda term: atom),
+    ('atom : NAME', lambda name: name),
+    ('atom : NUMBER', lambda x: float(x)),
+    ('atom : OPEN_BRACKET CLOSE_BRACKET', lambda open_bracket, close_bracket: ()),
+
 ], token_list)
 
+print_ast = pprint.PrettyPrinter(width=60, compact=True)
 
-@click.command()
-@click.argument('source_file',type=click.File('r'))
-def build(source_file):
+tokens = lexer(source)
+tokens = [value for value in tokens if str(value)[:7] != 'COMMENT' and str(value)[:8] != 'NEW_LINE']
+ast = parser(tokens)
 
-    code = source_file.read()
-  
-    tokens = lexer(code)
-    ast = parser(tokens)
-    pprint.pprint(ast)
+print_ast.print(ast)
 
-if __name__ == '__main__':
-     build()
 
 
 
